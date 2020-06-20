963c6b5293f4880032f81b5bf4242a7dc70fbbdc
Alessandra <alessamica@gmail.com>
2020-06-19 23:06:06 -0400

    Sound Component

---
+++ src/Components/Sound.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Sound() {
+  return (
+    <div>
+      <h1>App Sound</h1>
+      <button id='reset'>Reset</button>
+      <button id='start_stop'>Play/Pause</button>
+      <button id='info'>Info</button>
+    </div>
+  );
+}
+
+export default Sound;
