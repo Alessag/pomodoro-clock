6c9cc235224fc16f37c66e543e27dc7a9bb97bf9
Alessandra <alessamica@gmail.com>
2020-06-23 17:36:42 -0400

    Sound component

--- src/Components/Sound.js
+++ src/Components/Sound.js
@@ -4,9 +4,6 @@ function Sound() {
   return (
     <div>
       <h1>App Sound</h1>
-      <button id='reset'>Reset</button>
-      <button id='start_stop'>Play/Pause</button>
-      <button id='info'>Info</button>
     </div>
   );
 }
