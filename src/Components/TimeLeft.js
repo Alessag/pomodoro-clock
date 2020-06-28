e44e972d997e411606e9bc1ff15c8b721092cb6e
Alessandra <alessamica@gmail.com>
2020-06-28 00:00:13 -0400

    Props validation

--- src/Components/TimeLeft.js
+++ src/Components/TimeLeft.js
@@ -1,23 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 import momentDurationFormatSetup from 'moment-duration-format';
-import './styles/index.css';
+// import './styles/index.css';
 
 momentDurationFormatSetup(moment);
 
-function TimeLeft({ sessionLengthInSeconds }) {
-  const [timeLeft, setTimeLeft] = useState(sessionLengthInSeconds);
-
-  useEffect(() => {
-    setTimeLeft(sessionLengthInSeconds);
-  }, [sessionLengthInSeconds]);
-
-  const formattedTimeLeft = moment.duration(timeLeft, 's').format('mm:ss');
+function TimeLeft({ timeLeft, timerLabel }) {
+  const formattedTimeLeft = moment
+    .duration(timeLeft, 's')
+    .format('mm:ss', { trim: false });
 
   return (
     <div className='timer'>
-      <h1 id='timer-label'>Timer - Session</h1>
+      <h1 id='timer-label'>{timerLabel}</h1>
       <span className='timer__sesion'>Work</span>
       <span id='time-left' className='timer__countdown'>
         {formattedTimeLeft}
@@ -27,7 +23,7 @@ function TimeLeft({ sessionLengthInSeconds }) {
 }
 
 TimeLeft.propTypes = {
-  sessionLengthInSeconds: PropTypes.number.isRequired,
+  timeLeft: PropTypes.number.isRequired,
+  timerLabel: PropTypes.number.isRequired,
 };
-
 export default TimeLeft;
