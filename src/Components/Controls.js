f150dd751e7aaa6891cccf28f5132d461e454cc8
Alessandra <alessamica@gmail.com>
2020-07-04 23:38:04 -0400

    Icons and modal component incorporated

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -2,44 +2,58 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { IoMdInformationCircleOutline } from 'react-icons/io';
 import { MdReplay } from 'react-icons/md';
-import { GrPlayFill } from 'react-icons/gr';
-import { BsFillPauseFill } from 'react-icons/bs';
+import { BsPlayFill, BsFillPauseFill } from 'react-icons/bs';
+import Modal from './Modal/Modal';
 
-function Controls({ isStarted, handleStartStopClick, handleResetButtonClick }) {
+function Controls({
+  isStarted,
+  handleStartStopClick,
+  handleResetButtonClick,
+  handleInfoButtonClick,
+  showModal,
+}) {
   return (
-    <div className='controls'>
+    <div className='flex items-center justify-center'>
       <button
         type='button'
         id='reset'
-        className='controls__btn'
         onClick={handleResetButtonClick}
+        className='flex items-center justify-center w-12 h-12 border-solid border border-white rounded-full'
       >
-        <MdReplay id='reset' className='controls__icons' />
+        <MdReplay id='reset' className='text-center' />
       </button>
-
       <button
         onClick={handleStartStopClick}
         type='button'
         id='start_stop'
-        className='controls__btn controls__btn-md'
+        className='w-16 h-16 border-solid border border-white rounded-full m-6 flex items-center justify-center'
       >
         {isStarted ? (
-          <BsFillPauseFill id='start_stop' className='controls__icons' />
+          <BsFillPauseFill id='start_stop' />
         ) : (
-          <GrPlayFill id='start_stop' className='controls__icons' />
+          <BsPlayFill id='start_stop' color='white' size='2em' />
         )}
       </button>
-
-      <button type='button' id='info' className='controls__btn'>
-        <IoMdInformationCircleOutline id='info' className='controls__icons' />
+      <button
+        onClick={handleInfoButtonClick}
+        type='button'
+        id='info'
+        className='flex items-center justify-center w-12 h-12 border-solid border border-white rounded-full'
+      >
+        <IoMdInformationCircleOutline id='info' />
       </button>
+      {showModal ? (
+        <Modal handleInfoButtonClick={handleInfoButtonClick} />
+      ) : null}
     </div>
   );
 }
 
 Controls.propTypes = {
-  isStarted: PropTypes.string.isRequired,
+  isStarted: PropTypes.bool.isRequired,
   handleStartStopClick: PropTypes.func.isRequired,
   handleResetButtonClick: PropTypes.func.isRequired,
+  handleInfoButtonClick: PropTypes.func.isRequired,
+  showModal: PropTypes.bool.isRequired,
 };
 export default Controls;
